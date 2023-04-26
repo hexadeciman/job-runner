cf3f64bb93cfeaea6f48b2cbf6cce91f0b4c21e6
root <yassinebenchekroune@gmail.com>
2023-04-26 22:37:15 +0000

    Add code for project MPV

--- pinger/src/ping.sh
+++ pinger/src/ping.sh
@@ -6,7 +6,7 @@ while true; do
     for var in $(env | grep '^API_URL_' | cut -d= -f1); do
         url=$(eval "echo \$$var") # Get the value of the environment variable
         echo "Making request to $url"
-        curl -s $url
+        curl -s --max-time 45 $url
         sleep 2
     done
     sleep 60
